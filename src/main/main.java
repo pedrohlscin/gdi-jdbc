bd86a9be764585aa7d3b37e448731dea5010c221
pedro <phls73376@gmail.com>
2018-11-01 10:08:04 -0300

    list all images

--- src/main/main.java
+++ src/main/main.java
@@ -6,6 +6,9 @@ import poco.Image;
 import poco.Pessoa;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Date;
@@ -44,15 +47,36 @@ public class main {
 //        ArrayList<Pessoa> pessoas = dao.getPessoas();
 //        for (Pessoa pessoa : pessoas) System.out.println(pessoa);
 
-        ImageDAO dao = new ImageDAO();
-        String nome = "image1.jpeg";
-        String filepath = "C:\\Users\\pedro\\Desktop\\dick-vigarista.jpg";
+//        recuperando imagens da database
+//        ImageDAO dao = new ImageDAO();
+//        String filepath = "C:\\Users\\pedro\\Desktop\\";
+//
+//        try {
+//        ArrayList<Image> list = dao.getImages();
+//            for (Image img : list ) {
+//                byte barr[] = img.blob.getBytes(1, (int)img.blob.length());
+//                FileOutputStream fos = new FileOutputStream(filepath + img.name);
+//                fos.write(barr);
+//                fos.close();
+//            }
+//
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        } catch (FileNotFoundException e) {
+//            e.printStackTrace();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
 
-        try {
-            if (dao.insert(nome, filepath))
-                System.out.println("Blob inserted!");
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+
+//        insert blob on database
+//        String filepath = "C:\\Users\\pedro\\Desktop\\";
+//        String nome = "image1.jpeg";
+//        try {
+//            if (dao.insert(nome, filepath))
+//                System.out.println("Blob inserted!");
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
     }
 }
